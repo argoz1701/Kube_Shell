1a2de854648d725ee50927e3f3f40b4c32a60b1f
Daniel <69382818+argoz1701@users.noreply.github.com>
2026-03-12 14:44:18 -0400

    Delete kube_shell_launch.py

--- kube_shell_launch.py
+++
@@ -1,9 +0,0 @@
-import subprocess, sys, os
-
-python = sys.executable
-script = os.path.join(os.path.dirname(__file__), "kub_shell.py")
-
-subprocess.Popen(
-    ["powershell", "-NoExit", "-Command", f"{python} '{script}' --shell"],
-    creationflags=subprocess.CREATE_NEW_CONSOLE
-)
